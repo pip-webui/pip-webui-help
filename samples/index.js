76b246f746a171ed486c95973677287bab3a29fa
KateNegrienko <katya@negrienko.com>
2016-05-18 17:05:08 +0300

    fix help

--- samples/index.js
+++ samples/index.js
@@ -2,34 +2,29 @@
  * @file Sample application to provide end-to-end example of WebUI
  * @copyright Digital Living Software Corp. 2014-2016
  */
- 
+
 /* global angular */
 
 (function () {
     'use strict';
-    
+
     var thisModule = angular.module('pipSample', [
         // 3rd Party Modules
         'ui.router', 'ui.utils', 'ngResource', 'ngAria', 'ngCookies', 'ngSanitize', 'ngMessages',
         'ngMaterial', 'wu.masonry', 'LocalStorageModule', 'angularFileUpload', 'ngAnimate',
         // Application Configuration must go first
-        'pipSampleConfig', 
+        'pipSampleConfig',
         // Modules from WebUI Framework
         'pipCore', 'pipRest', 'pipData', 'pipBasicControls', 'pipLayout', 'pipNav',
-         // Sample Application Modules
+        // Sample Application Modules
         'pipEntry', 'pipRest.State', 'pipHelp'
     ]);
 
-
-    thisModule.run(function(pipRest) {
-        pipRest.serverUrl('http://alpha.pipservices.net');
-    });
-
     thisModule.controller('pipSampleController',
-        function($scope, $rootScope) {
+        function ($scope, $rootScope) {
 
         }
     );
-    
+
 })();
 
