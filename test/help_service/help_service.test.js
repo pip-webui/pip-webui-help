faf5a09d8b79c2e347ac100e048c3f9f63a3501f
KateNegrienko <katya@negrienko.com>
2016-06-24 16:13:23 +0300

    help service test

--- test/help_service/help_service.test.js
+++ test/help_service/help_service.test.js
@@ -43,84 +43,94 @@ describe('HelpService', function () {
         service = pipHelp;
     }));
 
-    it('should be able to add new page and get list of added pages', function () {
-        service.addPage(page1);
+    describe('Add pages', function () {
+        it('should be able to add new page and get list of added pages', function () {
+            service.addPage(page1);
 
-        service.addPage({
-            state: 'test2',
-            visible: false,
-            access: access,
-            stateConfig: {}
-        });
-
-        expect(service.getDefaultPage().state).to.equal('help.' + page1.state);
-        expect(service.getDefaultPage().title).to.equal(page1.title);
-        expect(service.getDefaultPage().stateConfig.url).to.equal(page1.stateConfig.url);
-        expect(service.getDefaultPage().stateConfig.template).to.equal(page1.stateConfig.template);
-
-        expect(function () {
             service.addPage({
-                state: 'test',
+                state: 'test2',
+                visible: false,
+                access: access,
                 stateConfig: {}
             });
-        }).to.throw(Error);
+
+            expect(function () {
+                service.addPage({
+                    state: 'test',
+                    stateConfig: {}
+                });
+            }).to.throw(Error);
+        });
     });
 
-    it('should be able to add new page and get list of added pages', function () {
+    describe('Default state', function () {
+        it('default state', function () {
 
-        service.addPage(page1);
+            service.addPage(page1);
 
-        service.addPage({
-            state: 'test2',
-            visible: false,
-            access: access,
-            stateConfig: {}
+            service.addPage({
+                state: 'test2',
+                visible: false,
+                access: access,
+                stateConfig: {}
+            });
+
+            expect(service.getDefaultPage().state).to.equal('help.' + page1.state);
+            expect(service.getDefaultPage().title).to.equal(page1.title);
+            expect(service.getDefaultPage().stateConfig.url).to.equal(page1.stateConfig.url);
+            expect(service.getDefaultPage().stateConfig.template).to.equal(page1.stateConfig.template);
+
+            service.setDefaultPage('test2');
+
+            expect(stateSpy.called).to.isDefined;
+
+            expect(service.getDefaultPage().state).to.equal('help.test2');
+
+            expect(function () {
+                service.setDefaultPage('abc');
+            }).to.throw(Error);
+        });
+    });
+
+    describe('Get pages', function () {
+        it('if have some pages', function () {
+            service.addPage(page1);
+            service.addPage({
+                state: 'test2',
+                visible: false,
+                access: access,
+                stateConfig: {}
+            });
+
+            expect(service.getPages().length).to.equal(2);
         });
 
-        service.setDefaultPage('test2');
-
-        expect(stateSpy.called).to.isDefined;
-
-        expect(service.getDefaultPage().state).to.equal('help.test2');
-
-        expect(function () {
-            service.setDefaultPage('abc');
-        }).to.throw(Error);
-    });
-
-    it('should be get pages after add', function () {
-        var pages;
-
-        service.addPage(page1);
-
-        service.addPage({
-            state: 'test2',
-            visible: false,
-            access: access,
-            stateConfig: {}
+        it('if pages variables empty', function () {
+            expect(service.getPages().length).to.equal(0);
         });
-
-        pages = service.getPages();
-
-        expect(pages.length).to.equal(2);
     });
 
-    it('should be get errors if validation fall', function () {
-        expect(function () {
-            service.addPage();
-        }).to.throw('Invalid object');
-
-        expect(function () {
-            service.addPage({});
-        }).to.throw('Page should have valid Angular UI router state name');
-
-        expect(function () {
-            service.addPage({state:'test', access: true});
-        }).to.throw('"access" should be a function');
-
-        expect(function () {
-            service.addPage({state:'test', access: angular.noop});
-        }).to.throw('Invalid state configuration object');
+    describe('Validation function', function () {
+        it('should be get errors if invalid object', function () {
+            expect(function () {
+                service.addPage();
+            }).to.throw('Invalid object');
+        });
+        it('should be get errors if page should have valid Angular UI router state name', function () {
+            expect(function () {
+                service.addPage({});
+            }).to.throw('Page should have valid Angular UI router state name');
+        });
+        it('should be get errors if "access" should be a function', function () {
+            expect(function () {
+                service.addPage({state: 'test', access: true});
+            }).to.throw('"access" should be a function');
+        });
+        it('should be get errors if invalid state configuration object', function () {
+            expect(function () {
+                service.addPage({state: 'test', access: angular.noop});
+            }).to.throw('Invalid state configuration object');
+        });
     });
 
 });
