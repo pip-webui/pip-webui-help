ceae968ecfe1f2291ea0c3a0c30114e06f0f2269
Sergey Seroukhov <seroukhov@gmail.com>
2017-03-08 19:35:34 -0700

    'Full rebuilt'

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -4,5 +4,5 @@
 /// <reference path="angular-ui-router/angular-ui-router.d.ts" />
 /// <reference path="angular-material/angular-material.d.ts" />
 /// <reference path="core-js/core-js.d.ts" />
-/// <reference path="../lib/pip-webui-nav.d.ts" />
+/// <reference path="../../pip-webui-nav/dist/pip-webui-nav.d.ts" />
 
