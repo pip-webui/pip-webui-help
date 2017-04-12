35b042e30c5286a604896c61a076d5d869970bb0
Anastas Fonotov <anastasf@gmail.com>
2017-04-12 22:50:05 +0300

    'rebuild'

--- dist/pip-webui-help.d.ts
+++ dist/pip-webui-help.d.ts
@@ -1,38 +1,55 @@
 declare module pip.help {
 
 
-export class HelpTab {
-    state: string;
-    title: string;
-    index: string;
-    access: boolean;
-    visible: boolean;
-    stateConfig: any;
-}
-export interface IHelpService {
-    getDefaultTab(): any;
-    showTitleText(newTitleText: any): any;
-    showTitleLogo(newTitleLogo: any): any;
-    setDefaultTab(name: string): any;
-    showNavIcon(value: any): any;
-    getTabs(): any;
-}
-export interface IHelpProvider extends ng.IServiceProvider {
-    getDefaultTab(): HelpTab;
-    addTab(tabObj: any): any;
-    setDefaultTab(name: string): void;
-    getFullStateName(state: string): string;
-}
 export class HelpConfig {
     defaultTab: string;
     tabs: HelpTab[];
     titleText: string;
-    titleLogo: boolean;
+    titleLogo: string;
     isNavIcon: boolean;
 }
 
+export class HelpPageSelectedTab {
+    tab: HelpTab;
+    tabIndex: number;
+    tabId: string;
+}
 
-function configureHelpPageRoutes($stateProvider: any): void;
+
+export class HelpStateConfig {
+    url: string;
+    auth: boolean;
+    templateUrl?: string;
+    template?: string;
+}
+
+export class HelpTab {
+    state: string;
+    title: string;
+    index: number;
+    access: Function;
+    visible: boolean;
+    stateConfig: HelpStateConfig;
+}
+
+export interface IHelpService {
+    getDefaultTab(): HelpTab;
+    showTitleText(newTitleText: string): string;
+    showTitleLogo(newTitleLogo: string): any;
+    setDefaultTab(name: string): void;
+    showNavIcon(value: boolean): boolean;
+    getTabs(): HelpTab[];
+}
+export interface IHelpProvider extends ng.IServiceProvider {
+    getDefaultTab(): HelpTab;
+    addTab(tabObj: HelpTab): void;
+    setDefaultTab(name: string): void;
+    getFullStateName(state: string): string;
+}
+
+
+
+
 
 }
 
